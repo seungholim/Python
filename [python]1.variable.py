5ac7a509b96172a0853e4d460fa9dbdeeedff208
Lim Seungho <ho4788@gmail.com>
2023-06-14 23:55:47 +0900

    Add files via upload

---
+++ [python]1.variable.py
@@ -0,0 +1,27 @@
+# 홍길동씨 개인정보
+
+name = "홍길동"
+age = 25
+height = 184.6
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+# 변수 이름 짓는 규칙
+# 1. 영문자(대, 소문자 구분), 숫자, 언더바, 한글 사용 가능
+# 2. 첫 자리는 숫자 사용 불가
+# 3. 예약어 사용 불가
+
+# Convention, 파이썬 PEP8
+# 1. 한 단어로 된 변수는 소문자로 적는다.
+# age, good, name
+
+# 2. 두 단어 이상의 변수는 언더바로 구분한다.
+# my_name, my_friend_name
+
+# 3. 예약어와 충돌하는 경우에는 뒤에 언더바를 붙인다.
+# if_, true_
+
+# 4. 상수의 경우 대문자와 언더바를 혼용하여 쓴다
+# TOTAL, MAXX_NUM, AVG
